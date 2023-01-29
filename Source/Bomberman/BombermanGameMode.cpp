b44a5032446501f32cbd1bda6e0ea10917bbf4a0
Sebastian Pieta <spieta2@gmail.com>
2023-01-29 20:28:24 +0100

    Initial commit

---
+++ Source/Bomberman/BombermanGameMode.cpp
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "BombermanGameMode.h"
+#include "BombermanPlayerController.h"
+#include "BombermanCharacter.h"
+#include "UObject/ConstructorHelpers.h"
+
+ABombermanGameMode::ABombermanGameMode()
+{
+	// use our custom PlayerController class
+	PlayerControllerClass = ABombermanPlayerController::StaticClass();
+
+	// set default pawn class to our Blueprinted character
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/TopDownCPP/Blueprints/TopDownCharacter"));
+	if (PlayerPawnBPClass.Class != nullptr)
+	{
+		DefaultPawnClass = PlayerPawnBPClass.Class;
+	}
+}
